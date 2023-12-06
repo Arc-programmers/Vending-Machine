20777b2f2079714e251c58b1999a24d14d39c333
Abhishekshenoy <abhishekshenoy2004@gmail.com>
2023-12-06 22:14:39 +0530

    copied from chat gpt

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+// Motor connections to Arduino pins
+const int ENA = 9;  // Enable Pin for Motor A
+const int IN1 = 8;  // Input Pin 1 for Motor A
+const int IN2 = 7;  // Input Pin 2 for Motor A
 
+void setup() {
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    int speed_percent = Serial.parseInt();
+    speed_percent = constrain(speed_percent, -100, 100); // Limit speed to -100 to 100
 
+    if (speed_percent > 0) {
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      analogWrite(ENA, map(speed_percent, 0, 100, 0, 255));
+    } else if (speed_percent < 0) {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      analogWrite(ENA, map(-speed_percent, 0, 100, 0, 255));
+    } else {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      analogWrite(ENA, 0);
+    }
+  }
 }
